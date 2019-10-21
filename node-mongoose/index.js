74a12c1519a6f24e39691a83216e00c22bab4215
rajat14323 <rajatkumarbind@gmail.com>
2019-10-21 12:29:26 +0530

    Monngose part3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -2,6 +2,43 @@
 
 
 
+//mongoose part  2
+
+
+// const mongoose = require('mongoose');
+
+// const Dishes = require('./models/dishes');
+
+// const url = 'mongodb://localhost:27017/conFusion';
+// const connect = mongoose.connect(url);
+
+// connect.then((db) => {
+
+//     console.log('Connected correctly to server');
+
+//     Dishes.create({
+//         name: 'Uthapizza',
+//         description: 'Test'
+//     })
+//     .then((dish) => {
+//         console.log(dish);
+        
+//         return Dishes.find({}).exec();
+//     })
+//     .then((dishes) => {
+//         console.log(dishes);
+
+//         return Dishes.remove({});
+//     })
+//     .then(() => {
+//         return mongoose.connection.close();
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+// });
+
 
 
 const mongoose = require('mongoose');
@@ -16,16 +53,32 @@ connect.then((db) => {
     console.log('Connected correctly to server');
 
     Dishes.create({
-        name: 'Uthapizza',
-        description: 'Test'
+        name: 'Uthappizza2',
+        description: 'test2'
     })
     .then((dish) => {
         console.log(dish);
-        
-        return Dishes.find({}).exec();
+
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
     })
-    .then((dishes) => {
-        console.log(dishes);
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
 
         return Dishes.remove({});
     })
@@ -34,6 +87,6 @@ connect.then((db) => {
     })
     .catch((err) => {
         console.log(err);
-    });
+    });s
 
 });
